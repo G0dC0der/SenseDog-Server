b7c7d4cc8f4104a7a9b534d857d99c9328d19cfa
Pojahn <pojahn@gmail.com>
2017-11-27 21:40:48 +0100

    Migrated endpoints to Cloud Endpoints

--- src/main/java/com/sensedog/rest/AlarmEndpoint.java
+++ src/main/java/com/sensedog/rest/AlarmEndpoint.java
@@ -1,12 +1,16 @@
 package com.sensedog.rest;
 
+import com.google.api.server.spi.config.Api;
+import com.google.api.server.spi.config.ApiIssuer;
+import com.google.api.server.spi.config.ApiMethod;
+import com.google.api.server.spi.config.ApiNamespace;
 import com.sensedog.detection.Severity;
 import com.sensedog.rest.model.ApiHealth;
+import com.sensedog.rest.model.ApiServiceCreate;
+import com.sensedog.rest.model.ApiSeverity;
 import com.sensedog.rest.model.request.ApiAlarmCreate;
 import com.sensedog.rest.model.request.ApiCloudUpdate;
 import com.sensedog.rest.model.request.ApiDetect;
-import com.sensedog.rest.model.ApiServiceCreate;
-import com.sensedog.rest.model.ApiSeverity;
 import com.sensedog.rest.model.request.ApiHealthReport;
 import com.sensedog.security.Token;
 import com.sensedog.service.AlarmService;
@@ -18,19 +22,25 @@ import org.apache.http.auth.AuthenticationException;
 
 import javax.inject.Inject;
 import javax.validation.Valid;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-@Path("/alarm")
-@Produces(MediaType.APPLICATION_JSON)
-@Consumes(MediaType.APPLICATION_JSON)
+@Api(
+    name = "alarm_endpoint",
+    version = "v1",
+    namespace =
+    @ApiNamespace(
+        ownerDomain = "sensedog.com",
+        ownerName = "sensedog.com",
+        packagePath = "com.sensedog.rest"
+    ),
+    issuers = {
+        @ApiIssuer(
+            name = "firebase",
+            issuer = "https://securetoken.google.com/YOUR-PROJECT-ID",
+            jwksUri = "https://www.googleapis.com/robot/v1/metadata/x509/securetoken@system.gserviceaccount.com")
+    }
+)
 public class AlarmEndpoint {
 
     private final AlarmService alarmService;
@@ -46,17 +56,16 @@ public class AlarmEndpoint {
         this.statusService = statusService;
     }
 
-    @POST
-    @Path("create")
+    @ApiMethod(name = "create_alarm_endpoint", path = "alarm/create", httpMethod = ApiMethod.HttpMethod.POST)
     public Response create(@Valid final ApiAlarmCreate request) {
         final AlarmServiceInfo alarmServiceInfo = alarmService.create(
-                request.getCloudToken(),
-                request.getDeviceModel(),
-                request.getOsVersion(),
-                request.getAppVersion(),
-                request.getCarrier(),
-                request.getServiceName(),
-                request.getBattery());
+            request.getCloudToken(),
+            request.getDeviceModel(),
+            request.getOsVersion(),
+            request.getAppVersion(),
+            request.getCarrier(),
+            request.getServiceName(),
+            request.getBattery());
 
         final ApiServiceCreate response = new ApiServiceCreate();
         response.setAlarmAuthToken(alarmServiceInfo.getAlarmAuthToken());
@@ -65,14 +74,13 @@ public class AlarmEndpoint {
         return Response.ok(response).build();
     }
 
-    @POST
-    @Path("detection")
+    @ApiMethod(name = "alarm_endpoint_detect", path = "alarm/detection", httpMethod = ApiMethod.HttpMethod.POST)
     public Response detect(@Valid final ApiDetect request,
                            @HeaderParam("alarm-auth-token") final String authToken) throws AuthenticationException {
         final Severity severity = alarmService.detect(
-                new Token.Alarm(authToken),
-                request.getDetectionType(),
-                request.getValue());
+            new Token.Alarm(authToken),
+            request.getDetectionType(),
+            request.getValue());
 
         final ApiSeverity response = new ApiSeverity();
         response.setSeverity(severity);
@@ -80,48 +88,42 @@ public class AlarmEndpoint {
         return Response.ok(response).build();
     }
 
-    @POST
-    @Path("stop")
+    @ApiMethod(name = "stop_alarm_endpoint", path = "alarm/stop", httpMethod = ApiMethod.HttpMethod.POST)
     public Response stop(@HeaderParam("alarm-auth-token") final String authToken) {
         alarmService.stop(new Token.Alarm(authToken));
 
         return Response.noContent().build();
     }
 
-    @POST
-    @Path("start")
+    @ApiMethod(name = "start_alarm_endpoint", path = "alarm/start", httpMethod = ApiMethod.HttpMethod.POST)
     public Response start(@HeaderParam("alarm-auth-token") final String authToken) {
         alarmService.start(new Token.Alarm(authToken));
 
         return Response.noContent().build();
     }
 
-    @POST
-    @Path("destroy")
+    @ApiMethod(name = "destroy_alarm_endpoint", path = "alarm/destroy", httpMethod = ApiMethod.HttpMethod.POST)
     public Response destroy(@HeaderParam("alarm-auth-token") final String authToken) {
-        //TODO: Removes the entire service.
-        //Client should also remove their auth token when this endpoint is called.
+        //TODO: Removes the entire service. Client should also remove their auth token when this endpoint is called.
+
         return null;
     }
 
-    @PUT
-    @Path("update/cloud")
+    @ApiMethod(name = "alarm_endpoint_update_cloud_token", path = "alarm/update/cloud", httpMethod = ApiMethod.HttpMethod.POST)
     public Response updateCloud(@HeaderParam("alarm-auth-token") final String authToken, final ApiCloudUpdate request) {
         userService.updateAlarmCloudToken(new Token.Alarm(authToken), request.getCloudToken());
 
         return Response.noContent().build();
     }
 
-    @POST
-    @Path("status")
+    @ApiMethod(name = "put_alarm_endpoint_status", path = "alarm/status", httpMethod = ApiMethod.HttpMethod.POST)
     public Response report(@HeaderParam("alarm-auth-token") final String alarmAuthToken, @Valid final ApiHealthReport request) {
         statusService.report(new Token.Alarm(alarmAuthToken), request.getBattery());
 
         return Response.noContent().build();
     }
 
-    @GET
-    @Path("status")
+    @ApiMethod(name = "read_alarm_endpoint_status", path = "alarm/status", httpMethod = ApiMethod.HttpMethod.GET)
     public Response readStatus(@HeaderParam("alarm-auth-token") final String alarmAuthToken) {
         final HealthStatus healthStatus = statusService.read(new Token.Alarm(alarmAuthToken));
 
--- src/main/java/com/sensedog/rest/MasterEndpoint.java
+++ src/main/java/com/sensedog/rest/MasterEndpoint.java
@@ -1,14 +1,19 @@
 package com.sensedog.rest;
 
+import com.google.api.server.spi.config.Api;
+import com.google.api.server.spi.config.ApiIssuer;
+import com.google.api.server.spi.config.ApiMethod;
+import com.google.api.server.spi.config.ApiNamespace;
+import com.google.api.server.spi.config.Named;
 import com.sensedog.rest.convert.EntryConverter;
 import com.sensedog.rest.model.ApiHealth;
+import com.sensedog.rest.model.ApiServices;
+import com.sensedog.rest.model.ApiToken;
 import com.sensedog.rest.model.request.ApiCloudUpdate;
 import com.sensedog.rest.model.request.ApiConnect;
 import com.sensedog.rest.model.request.ApiInvite;
 import com.sensedog.rest.model.request.ApiLogin;
 import com.sensedog.rest.model.request.ApiMasterUserCreate;
-import com.sensedog.rest.model.ApiServices;
-import com.sensedog.rest.model.ApiToken;
 import com.sensedog.security.Token;
 import com.sensedog.service.StatusService;
 import com.sensedog.service.UserService;
@@ -17,22 +22,27 @@ import com.sensedog.service.model.ServiceData;
 
 import javax.inject.Inject;
 import javax.validation.Valid;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Path("/master")
-@Produces(MediaType.APPLICATION_JSON)
-@Consumes(MediaType.APPLICATION_JSON)
+@Api(
+    name = "master_endpoint",
+    version = "v1",
+    namespace =
+    @ApiNamespace(
+        ownerDomain = "sensedog.com",
+        ownerName = "sensedog.com",
+        packagePath = "com.sensedog.rest"
+    ),
+    issuers = {
+        @ApiIssuer(
+            name = "firebase",
+            issuer = "https://securetoken.google.com/YOUR-PROJECT-ID",
+            jwksUri = "https://www.googleapis.com/robot/v1/metadata/x509/securetoken@system.gserviceaccount.com")
+    }
+)
 public class MasterEndpoint {
 
     private final UserService userService;
@@ -45,8 +55,7 @@ public class MasterEndpoint {
         this.statusService = statusService;
     }
 
-    @POST
-    @Path("new")
+    @ApiMethod(name = "master_endpoint_new", path = "master/new", httpMethod = ApiMethod.HttpMethod.POST)
     public Response newMasterUser(@Valid final ApiMasterUserCreate request) {
         userService.createMasterUser(
                 request.getName(),
@@ -57,17 +66,16 @@ public class MasterEndpoint {
         return Response.noContent().build();
     }
 
-    @POST
-    @Path("login")
+    @ApiMethod(name = "master_endpoint_login", path = "master/login", httpMethod = ApiMethod.HttpMethod.POST)
     public Response login(final ApiLogin request) {
         userService.login(request.getEmail());
 
         return Response.noContent().build();
     }
 
-    @POST
-    @Path("connect")
-    public Response connect(@Valid final ApiConnect request) { //TODO: Nu kan vem som helst ansluta till en service. Vi måste verifera att personens email verkligen tillhör honom.
+    //TODO: Currently anyone may join a service. We have to verify that the user has been invited.
+    @ApiMethod(name = "master_endpoint_connect", path = "master/connect", httpMethod = ApiMethod.HttpMethod.POST)
+    public Response connect(@Valid final ApiConnect request) {
         final String token = userService.connectToService(request.getPinCode(), request.getEmail());
 
         final ApiToken apiToken = new ApiToken();
@@ -76,8 +84,7 @@ public class MasterEndpoint {
         return Response.ok(apiToken).build();
     }
 
-    @POST
-    @Path("invite")
+    @ApiMethod(name = "master_endpoint_invite", path = "master/invite", httpMethod = ApiMethod.HttpMethod.POST)
     public Response invite(@HeaderParam("master-auth-token") final String authToken,
                            @Valid final ApiInvite request) {
         userService.invite(
@@ -89,9 +96,8 @@ public class MasterEndpoint {
         return Response.noContent().build();
     }
 
-    @GET
-    @Path("view/all/{email}")
-    public Response viewAll(@PathParam("email") final String email) {
+    @ApiMethod(name = "master_endpoint_view_all", path = "master/view/all/{email}", httpMethod = ApiMethod.HttpMethod.GET)
+    public Response viewAll(@Named("email") final String email) {
         final List<ServiceData> serviceInfos = userService.viewAll(email);
 
         final ApiServices apiServices = new ApiServices();
@@ -100,31 +106,27 @@ public class MasterEndpoint {
         return Response.ok(apiServices).build();
     }
 
-    @POST
-    @Path("resume")
+    @ApiMethod(name = "master_endpoint_resume", path = "master/resume", httpMethod = ApiMethod.HttpMethod.POST)
     public Response resume(@HeaderParam("master-auth-token") final String authToken) {
         userService.resume(new Token.Master(authToken));
 
         return Response.noContent().build();
     }
 
-    @POST
-    @Path("pause")
+    @ApiMethod(name = "master_endpoint_pause", path = "master/pause", httpMethod = ApiMethod.HttpMethod.POST)
     public Response pause(@HeaderParam("master-auth-token") final String authToken) {
         userService.pause(new Token.Master(authToken));
         return Response.noContent().build();
     }
 
-    @PUT
-    @Path("update/cloud")
+    @ApiMethod(name = "master_endpoint_update_cloud_token", path = "master/update/cloud", httpMethod = ApiMethod.HttpMethod.POST)
     public Response updateCloud(@HeaderParam("master-auth-token") final String authToken, final ApiCloudUpdate request) {
         userService.updateMasterCloudToken(new Token.Master(authToken), request.getCloudToken());
 
         return Response.noContent().build();
     }
 
-    @GET
-    @Path("status")
+    @ApiMethod(name = "master_endpoint_status", path = "master/status", httpMethod = ApiMethod.HttpMethod.GET)
     public Response read(@HeaderParam("master-auth-token") final String masterAuthToken) {
         final HealthStatus healthStatus = statusService.read(new Token.Master(masterAuthToken));
 
